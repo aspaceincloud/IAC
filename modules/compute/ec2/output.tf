4cf0c8ba3620df5e7ad871035b6bd4c5acbe4fc5
Abhishek BHOLA (contractor) <abhishek.bhola@amadeus.com>
2024-02-25 19:17:59 +0530

    Revert "Moving Project-A to the Git Remote Repository"
    This reverts commit 01b9a085342ba59ebb05da4444512e7852803b8d.

--- modules/compute/ec2/output.tf
+++
@@ -1,9 +0,0 @@
-/*
-output "web_pub_servers_name" {
-    value = aws_instance.web_pub_servers[each.key].public_ip
-}
-
-output "web_pri_servers_name" {
-    value = aws_instance.pri_servers[each.key].private_ip
-}
-*/
--- modules/network/vpc/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "region" {}
-variable "project_name" {}
-variable "vpc_cidr" {}
-variable "public_subnet_a_cidr" {}
-variable "public_subnet_b_cidr" {}
-variable "private_subnet_a_cidr" {}
-variable "private_subnet_b_cidr" {}
